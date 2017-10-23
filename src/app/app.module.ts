818a8b2d77d710675039bba1de5a3b32e304f798
Udara Sampath Bandara <usbgalle@gmail.com>
2017-10-23 19:14:28 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule }  from '@angular/http';
+
+import "rxjs/add/operator/map";
+
+import {ChannelService, ChannelConfig, SignalrWindow} from "./channel.service";
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+
+let channelConfig = new ChannelConfig();  
+channelConfig.url = "http://localhost:9123/signalr";  
+channelConfig.hubName = "EventHub";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ChannelService, 
+    { provide: SignalrWindow, useValue: window },
+    { provide: 'channel.config', useValue: channelConfig }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
